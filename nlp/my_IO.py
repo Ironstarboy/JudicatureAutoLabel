6fc6807e73659d6a27b29ca0c8e07e6aee0877af
Ironstarboy <52195769+Ironstarboy@users.noreply.github.com>
2022-01-19 02:21:31 +0800

    feat:autoAbstract docs:摘要

--- nlp/my_IO.py
+++ nlp/my_IO.py
@@ -1,12 +1,12 @@
+import os
 def readfile(filename):
     content = ''
     encodings = ['utf-8', 'GBK', 'gb2312']
-    index = -1
+    index = 0
     while 1:
         if content != '' or index == len(encodings):
             break
         try:
-
             f = open(filename, 'r+', encoding=encodings[index])
             content = f.read()
             f.close()
@@ -19,3 +19,22 @@ def readfile(filename):
     if content == '':
         print('{}读取失败'.format(filename))
     return content
+
+def mkdir(dirPath):
+    if not os.path.exists(dirPath):
+        os.makedirs(dirPath)
+
+# 获取文件列表，该目录下放着一同个类别的文档,数量为几百份。不进行递归获取
+def getFilelist(path):
+    filelist = []
+    files = os.listdir(path)
+    for f in files:
+        if (f[0] == '.'):
+            pass
+        else:
+            filelist.append(f)
+    return filelist
+
+def saveFile(file_path, content:str):
+    with open(file_path,'w',encoding='utf-8') as f:
+        f.write(content)
